6d427e2f36cdb7eb76eb60540975f1351136254e
Vu Viet Hong Quan <vuviethongquan@gmail.com>
2024-05-31 16:31:52 +0700

    .

--- client/pizzaorder/lib/main.dart
+++ client/pizzaorder/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:pizzaorder/components/pizza_card_gird.dart';
 import 'package:pizzaorder/pages/all_product_page.dart';
 import 'package:pizzaorder/pages/favorites_page.dart';
 
-import 'ui/get_started.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,13 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-<<<<<<< HEAD
       home: Scaffold(body: AllProductPage()),
-=======
-      title: 'DOMINI PIZZA',
-      home: GetStarted(),
-      debugShowCheckedModeBanner: false,
->>>>>>> ca086a72241af67e7e0802b0b3fc697a1cf12ecc
     );
   }
 }
